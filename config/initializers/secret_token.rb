0fc98ee9f6842a00e7540b9f92896eeabe45ce10
Yasyf Mohamedali <yasyfm@gmail.com>
2014-08-16 16:55:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peoplefindr::Application.config.secret_key_base = '2ffecdb65a92b7adf3663501def703b85da0d7d8a7d24357f781593ac78e7bb7b29e9de080298e458f46d2ba3b0edf2fd21340873f93772c193a466217600009'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Peoplefindr::Application.config.session_store :cookie_store, key: '_peoplefindr_session'
